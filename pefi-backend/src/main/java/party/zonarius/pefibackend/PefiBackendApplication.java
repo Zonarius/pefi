f0b2407c04e271834f5293ba5dcc45dcf758393f
Zonarius <zonarius@live.at>
2022-08-14 10:14:25 +0200

    Fix default Iban

--- pefi-backend/src/main/java/party/zonarius/pefibackend/PefiBackendApplication.java
+++ pefi-backend/src/main/java/party/zonarius/pefibackend/PefiBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = { "classpath:application.properties", "classpath:secret.properties" })
 public class PefiBackendApplication {
 
 	public static void main(String[] args) {
